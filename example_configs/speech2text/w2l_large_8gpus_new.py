Gated Linear Units Bug Fix

example_configs/speech2text/w2l_large_8gpus_new.py
@@ -35,12 +35,16 @@ base_params = {
     "power": 2.0,
   },
 
+  "larc_params": {
+    "larc_eta": 0.001,
+  },
+
   "regularizer": tf.contrib.layers.l2_regularizer,
   "regularizer_params": {
     'scale': 0.0005
   },
 
-  "max_grad_norm": 5.0,
+  #"max_grad_norm": 5.0,
   "dtype": "mixed",
   "loss_scaling": "Backoff",
 